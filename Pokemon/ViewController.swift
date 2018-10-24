530b65d348c123f3f93d093b410fa145c7d01c41
Emily Baker-King <ebakerking@gmail.com>
2018-10-24 09:19:52 -0500

    Added files and POD

---
+++ Pokemon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by Emily Baker-King on 10/23/18.
+//  Copyright © 2018 Emily Baker-King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
